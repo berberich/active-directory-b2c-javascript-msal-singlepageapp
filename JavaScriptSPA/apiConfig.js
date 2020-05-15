c3d1c94d5aee05e97ba7e9e1bf646deb6ab0e1b9
Jason Berberich <jason.berberich@microsoft.com>
2020-05-15 01:45:59 -0500

    Update apiConfig.js
    updated to use /api instead of /helloapi

--- JavaScriptSPA/apiConfig.js
+++ JavaScriptSPA/apiConfig.js
@@ -1,5 +1,5 @@
 // The current application coordinates were pre-registered in a B2C tenant.
 const apiConfig = {
-  b2cScopes: ["https://berberichb2c.onmicrosoft.com/helloapi/demo.read"],
+  b2cScopes: ["https://berberichb2c.onmicrosoft.com/api/demo.read"],
   webApi: "https://jaberber-nodeadddb2c.azurewebsites.net/hello"
 };
