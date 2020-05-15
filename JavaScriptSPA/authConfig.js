ccd53c0034d9f0fda266c4897c47fc1a1e2c355e
Jason Berberich <jason.berberich@microsoft.com>
2020-05-15 00:27:44 -0500

    Update authConfig.js
    Was using wrong clientID before.

--- JavaScriptSPA/authConfig.js
+++ JavaScriptSPA/authConfig.js
@@ -6,7 +6,7 @@
  * */
 const msalConfig = {
   auth: {
-    clientId: "5b735780-8db9-4b05-bd46-b109486b88cd",
+    clientId: "c6d74d9f-68b5-4f70-bae1-fcc0931e9949",
     authority: "https://berberichb2c.b2clogin.com/berberichb2c.onmicrosoft.com/B2C_1_signupsignin1",
     validateAuthority: false
   },
